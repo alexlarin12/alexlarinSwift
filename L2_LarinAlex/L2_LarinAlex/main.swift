88b50d4f6e1e82c59d42f714e920cb430bd94a0d
Alexlarin12 <alex12081973@mail.ru>
2019-06-24 11:06:43 +0300

    Lesson 2. функции и массивы

---
+++ L2_LarinAlex/L2_LarinAlex/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L2_LarinAlex
+//
+//  Created by пользователь on 21/06/2019.
+//  Copyright © 2019 Alex Larin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
